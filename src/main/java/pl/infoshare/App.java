e488ccbc9a217e218505e72cf4b6c6dd88af2c7a
Thyion <krzbeszczynski@gmail.com>
2018-10-10 17:30:22 +0200

    Initial commit

---
+++ src/main/java/pl/infoshare/App.java
@@ -0,0 +1,67 @@
+package pl.infoshare;
+
+
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+
+
+    public static void main(String[] args)
+    {
+
+
+        System.out.println( "Hello World!" );
+
+        System.out.println("--------------------");
+
+        String name = "Krzysztof";
+        String lastName = "Beszczynski";
+        System.out.print("Name and LastName : ");
+        System.out.println(name + " " + lastName);
+
+        System.out.println("--------------------");
+
+        byte age = 28;
+        String city = "Gdansk";
+        System.out.println("Age" +" : " + age);
+        System.out.println("City" + " : " + city);
+
+        System.out.println("Age" +" : " + age + " \n" + "City : " + city);
+
+        System.out.println("--------------------");
+
+        int a = 3;
+        int b = 4;
+
+
+
+        int sum = a+b;
+        int difference = a-b;
+        int ratio = a*b;
+        double quotient = (double)a/(double)b;
+        //int zero = a/0;
+
+        System.out.println("Sum (a+b) = " + sum);
+        System.out.println("Difference (a-b) = " +  difference);
+        System.out.println("Ratio (axb) = " + ratio);
+        System.out.println("Quotient (a/b) = " + quotient);
+
+
+        double function 
+
+        System.out.println("--------------------");
+
+
+
+
+
+
+
+
+    }
+}

aa5e6c0756cdab39df9d8e6c1cb7abc02fec8fdf
Thyion <krzbeszczynski@gmail.com>
2018-10-15 19:43:18 +0200

    Lesson3

--- src/main/java/pl/infoshare/Lesson3.java
+++ src/main/java/pl/infoshare/Lesson3.java
@@ -1,5 +1,6 @@
 package pl.infoshare;
 
+import java.sql.SQLOutput;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -8,12 +9,23 @@ public class Lesson3 {
 
     public static void main (String[] args) {
 
-
+/*
         for (short i = 1 ; i <= 1000 ;i++) {
             System.out.println(i);
         }
 
+        int [] [] array2D = new int[10][12];
 
+        for (int i = 1; i < array2D.length + 1; i++) {
+            System.out.println("I'm in row no. " + i);
+            for (int j = 1 ; j < array2D[0].length + 1; j++) { // dzieki temu [0] pobieramy dlugosc 2 wymiaru
+                System.out.println("I'm in column no. " + j);
+            }
+        }
+
+
+
+        // ------------------EXERCISES--------------------
 
         System.out.println("------------");
 
@@ -22,9 +34,6 @@ public class Lesson3 {
         System.out.println("------------");
 
         int[] numbers = {2,5,10,25,75,74,15,12,32,22};
-        // ????? jak lepiej ??? int numbers2[] = {1,2};
-
-
 
         for (int i = 0; i < numbers.length;i++)
             if (i%2==0){
@@ -65,26 +74,68 @@ public class Lesson3 {
         int askNumber;
         askNumber = in.nextInt();
 
-        if (askNumber > 10 || askNumber < 1) {
+        do {
+            if (askNumber > 10 || askNumber < 1) {
 
-            do {
-                System.out.println("Liczba miala byc od 1-10 !!! Wybierz jeszcze raz.");
-                in.reset();
-                askNumber = in.nextInt();
-            } while (askNumber > 10 || askNumber < 1);
-        }
+                do {
+                    System.out.println("Liczba miala byc od 1-10 !!! Wybierz jeszcze raz.");
+                    in.reset();
+                    askNumber = in.nextInt();
+                } while (askNumber > 10 || askNumber < 1);
+            } else {
+                do {
+                    System.out.println("Niestety nie trafiles !!!");
+                    System.out.println("Myslalem o liczbie: " + number);
+                    System.out.println("Pomyliles sie o: " + Math.abs(number-askNumber));
+                    System.out.println("Pomysle nowa liczbe. Sprobuj zgadnac jeszcze raz.");
+                    number = random.nextInt(10)+1;
+                    in.reset();
+                    askNumber = in.nextInt();
+                } while (askNumber!=number);
+            }
+        }while(askNumber!=number);
+
 
         System.out.println("Twoja propozycja to: " + askNumber);
         System.out.println("Liczba o której myslalem, to: " + number);
-        if (askNumber==number)
-            System.out.println("Gratulacje trafiles!!");
-        else
-            System.out.println("Pomyliles sie o: " + Math.abs(number-askNumber));
+        System.out.println("Gratulacje trafiles!!");
 
+*/
 
         // ------------------HOMEWORK--------------------
 
 
+        int[] chosenNumbers = new int[6];
+
+        System.out.println("Wytypuj 6 liczb w zakresie od 1 do 49");
+
+        for (int i = 0 ; i < chosenNumbers.length ; i++) {
+            System.out.println("Wybierz " + i+1 + " liczbe :");
+            chosenNumbers[i] = typeNumber();
+        }
+
+
+
+
+
+    }
+
+    private static int typeNumber() {
+        Scanner in = new Scanner(System.in);
+
+        int choseNumber;
+
+        do {
+            while (!in.hasNextInt()) {
+                    System.out.println("Liczba miala byc od 1-49 !!! Wybierz jeszcze raz.");
+                    in.nextInt();
+                }
+                choseNumber = in.nextInt();
+
+        }while(choseNumber > 49 || choseNumber < 1);
+
+
+        return choseNumber;
 
     }
 }

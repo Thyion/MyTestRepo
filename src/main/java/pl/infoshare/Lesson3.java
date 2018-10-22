e4bfc341b758d520bb52082ca91fdb09b8ca7136
Thyion <krzbeszczynski@gmail.com>
2018-10-22 20:03:17 +0200

    sdsdsdds

--- src/main/java/pl/infoshare/Lesson3.java
+++ src/main/java/pl/infoshare/Lesson3.java
@@ -1,6 +1,6 @@
 package pl.infoshare;
 
-import java.sql.SQLOutput;
+
 import java.util.Random;
 import java.util.Scanner;
 
@@ -13,54 +13,35 @@ public class Lesson3 {
         for (short i = 1 ; i <= 1000 ;i++) {
             System.out.println(i);
         }
-
         int [] [] array2D = new int[10][12];
-
         for (int i = 1; i < array2D.length + 1; i++) {
             System.out.println("I'm in row no. " + i);
             for (int j = 1 ; j < array2D[0].length + 1; j++) { // dzieki temu [0] pobieramy dlugosc 2 wymiaru
                 System.out.println("I'm in column no. " + j);
             }
         }
-
-
-
         // ------------------EXERCISES--------------------
-
         System.out.println("------------");
-
         System.out.println("Exercise no 1");
-
         System.out.println("------------");
-
         int[] numbers = {2,5,10,25,75,74,15,12,32,22};
-
         for (int i = 0; i < numbers.length;i++)
             if (i%2==0){
                 System.out.println(numbers[i]);
             }
-
         System.out.println("------------");
-
         System.out.println("Exercise no 2");
-
         System.out.println("------------");
-
         char[] [] chars = {{'*','*','*'},{'*','*','*'},{'*','*','*'},{'*','*','*'},{'*','*','*'},{'*','*','*'},{'*','*','*'},{'*','*','*'},{'*','*','*'},{'*','*','*'},};
-
         for (int i = 0; i < chars.length;i++) {
             if (i % 2 != 0)
                 System.out.println(chars[i]);
             else
                 System.out.println(" ");
         }
-
         System.out.println("------------");
-
         System.out.println("Exercise no 3");
-
         System.out.println("------------");
-
         Random random = new Random();
         int number = random.nextInt(10)+1;
         System.out.println("Zagrajmy w gre. Pomyslalem jakas liczbe calkowita od 1 do 10...\nZgadniesz jaka to liczba ??");
@@ -69,14 +50,11 @@ public class Lesson3 {
             System.out.println("That's not a number!!!");
             in.next(); // this is important!
         }
-
         System.out.print("Podaj liczbe: ");
         int askNumber;
         askNumber = in.nextInt();
-
         do {
             if (askNumber > 10 || askNumber < 1) {
-
                 do {
                     System.out.println("Liczba miala byc od 1-10 !!! Wybierz jeszcze raz.");
                     in.reset();
@@ -94,12 +72,9 @@ public class Lesson3 {
                 } while (askNumber!=number);
             }
         }while(askNumber!=number);
-
-
         System.out.println("Twoja propozycja to: " + askNumber);
         System.out.println("Liczba o której myslalem, to: " + number);
         System.out.println("Gratulacje trafiles!!");
-
 */
 
         // ------------------HOMEWORK--------------------
@@ -107,27 +82,61 @@ public class Lesson3 {
 
         int[] chosenNumbers = new int[6];
 
-        int[] random6Numbers = new int[6];
+        int[] randomNumber = new int[6];
 
-
+        randomNumber = randomNumbers();
 
         System.out.println("Wytypuj 6 liczb w zakresie od 1 do 49");
 
-        for (int i = 0 ; i < chosenNumbers.length ; i++) {
-            System.out.println("Wybierz " + (i+1) + " liczbe :");
+        chosenNumbers = typeNumbers();
 
-                int number = typeNumber();
-                chosenNumbers[i] = number;
+        int score = scores(chosenNumbers, randomNumber);
 
-        }
-
-
-
+        System.out.println("Liczba trafionych numerów : " + score);
+        System.out.println(wygrana(score));
 
 
     }
 
-    private static boolean czySieZawiera(int number, int[] chosenNumbers) {
+    private static int scores(int[] chosenNumbers, int[] randomNumbers) {
+
+        int scores = 0;
+
+        for (int i = 0; i<chosenNumbers.length; i++){
+            for (int j = 0; j < randomNumbers.length; j++){
+                if (chosenNumbers[i]==randomNumbers[j])
+                    scores++;
+            }
+        }
+
+
+        return scores;
+    }
+
+    private static int[] typeNumbers() {
+
+        int[] chosenumbers = new int[6];
+
+        for (int i = 0 ; i < chosenumbers.length ; i++) {
+
+            System.out.println("Wybierz " + (i+1) + " liczbe :");
+
+            int number = typeNumber();
+
+            if ((areIncluded(number, chosenumbers))==true){
+                System.out.println("Już taką liczbę wybrałeś. Wybierz jakąś inną.");
+                i--;
+            } else {
+                chosenumbers[i] = number;
+            }
+
+        }
+        System.out.println("Wybrałeś : " + chosenumbers[0] + " , " + chosenumbers[1] + " , " + chosenumbers[2] + " , " + chosenumbers[3] + " , " + chosenumbers[4] + " , " + chosenumbers[5]);
+
+        return chosenumbers;
+    }
+
+    private static boolean areIncluded(int number, int[] chosenNumbers) {
         for (int i =0 ; i < chosenNumbers.length;i++){
             if (chosenNumbers[i]==number) return true;
         }
@@ -144,9 +153,9 @@ public class Lesson3 {
         do {
 
             while (!in.hasNextInt()) {
-                    System.out.println("Liczba miala byc od 1-49 !!! Wybierz jeszcze raz.");
-                    in.nextInt();
-                }
+                System.out.println("Liczba miala byc od 1-49 !!! Wybierz jeszcze raz.");
+                in.nextInt();
+            }
             choseNumber = in.nextInt();
             if (choseNumber >49 || choseNumber < 1 )
                 System.out.println("Liczba miala byc od 1-49 !!! Wybierz jeszcze raz.");
@@ -156,5 +165,60 @@ public class Lesson3 {
 
         return choseNumber;
 
+    }
+
+    private static int[] randomNumbers() {
+        int[] randomNumbers = new int[6];
+
+
+
+        Random random = new Random();
+
+
+        for (int i = 0 ; i < randomNumbers.length ; i++) {
+
+            int number = random.nextInt(49)+1;
+
+            if ((areIncluded(number, randomNumbers))==true){
+
+                i--;
+            } else {
+                randomNumbers[i] = number;
+            }
+
+        }
+
+        return randomNumbers;
+    }
+
+    private static String wygrana(int number) {
+
+
+        switch (number) {
+            case 0:{
+                return "Niestety nic nie wygrałeś";
+            }
+            case 1: {
+                return "Niestety nic nie wygrałeś";
+            }
+            case 2:{
+                return "Niestety nic nie wygrałeś";
+            }
+            case 3:{
+                return "Wygrałes 20 zł !!!";
+            }
+            case 4:{
+                return "Wygrałeś 300 zł !!!";
+            }
+            case 5:{
+                return "Wygrałeś 8000 zł !!!";
+            }
+            case 6:{
+                return "GRATULACJE !!!! WYGRAŁEŚ 1 000 000 zł !!!!!";
+            }
+            default: {
+                return "default";
+            }
+        }
     }
 }
--- src/main/java/pl/infoshare/Lesson4.java
+++ src/main/java/pl/infoshare/Lesson4.java
@@ -1,4 +1,136 @@
 package pl.infoshare;
 
+import java.io.*;
+import java.nio.file.*;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Lesson4 {
+
+
+    public static void main(String[] args) throws IOException {
+
+        //TAK SIE ROBILO 8 LAT TEMU
+        /*
+        File file = new File("/home/krzysztofb/workspace/MyTestRepo/moj.txt");
+        Scanner in = new Scanner(file);
+        String line = in.nextLine();
+        String line2 = in.nextLine();
+        System.out.println(line);
+        System.out.println(line2);
+
+        PrintWriter zapis = new PrintWriter("/home/krzysztofb/workspace/MyTestRepo/moj2.txt");
+        zapis.print("Pierwsza linia");
+        zapis.close();
+        */
+        /*
+        System.out.println("----------------------------");
+        System.out.println("A tak sie robi teraz");
+
+        //FILES - NOWY PLIK
+
+        Files.createFile(Paths.get("/home/krzysztofb/workspace/MyTestRepo/moj3.txt"));
+
+        //FILES - NOWY FOLDER
+        Files.createDirectory((Paths.get("/home/krzysztofb/workspace/MyTestRepo/myFolder")));
+        */
+        //FILES - WCZYTYWANIE PLIKU \
+//        List<String> lines = Files.readAllLines(Paths.get("/home/krzysztofb/workspace/MyTestRepo/moj3.txt"));
+//        /System.out.println(lines);
+//        for (String lineInFile:lines){
+//            System.out.println(lineInFile);
+//        }
+//        String aa = lines.get(4);
+//        System.out.println(aa);
+//
+//        //KOPIOWANIE
+//
+//        Files.copy(
+//                Paths.get("/home/krzysztofb/workspace/MyTestRepo/moj3.txt"),
+//                Paths.get("/home/krzysztofb/Desktop/kopia.txt"),
+//                StandardCopyOption.REPLACE_EXISTING //pozwala na podmienienie istniejcegop pliku
+//                );
+//
+//        //------------------ ZADANIE 1 -------------------------
+//
+//        String[] nameArray = {"Janusz", "Tomasz", "Krzysztof", "Ewelina", "Karolina"};
+//
+//
+//        PrintWriter zapis = new PrintWriter(new FileWriter("/home/krzysztofb/workspace/MyTestRepo/nameArray.txt", true));
+//
+//        for (int i =0; i<nameArray.length;i++){
+//            zapis.println(nameArray[i]);
+//        }
+//
+//        //zapis.close();
+//
+//
+//        //------------------ ZADANIE 2 -------------------------
+//        System.out.println("Podaj imie ...");
+//        Scanner in = new Scanner(System.in);
+//        //zapis.println(in.nextLine());
+//
+//
+//        zapis.append(in.nextLine()); //append to jak println tylko nie zastepuje danych tylko dopisuje - ALE NIE DZIALA NIE WIEM CZEMU
+//
+//        zapis.close();
+//
+//        //------------------ ZADANIE 3 -------------------------
+//
+//        int [] losowane = new int[100];
+//        int [] losowane2 = new int[100];
+//        Random random = new Random();
+//
+//
+//
+//        PrintWriter zapisLosowan = new PrintWriter(new FileWriter("/home/krzysztofb/workspace/MyTestRepo/losowane.txt"));
+//        PrintWriter zapisLosowan2 = new PrintWriter(new FileWriter("/home/krzysztofb/workspace/MyTestRepo/losowane2.txt"));
+//
+//        for (int i =0 ; i<losowane.length;i++){
+//            zapisLosowan.println(random.nextInt(100)+1);
+//            zapisLosowan2.println(random.nextInt(100)+1);
+//        }
+//
+//        zapisLosowan.close();
+//        zapisLosowan2.close();
+//
+//
+//        List<String> listLos1 = Files.readAllLines(Paths.get("/home/krzysztofb/workspace/MyTestRepo/losowane.txt"));
+//        List<String> listLos2 = Files.readAllLines(Paths.get("/home/krzysztofb/workspace/MyTestRepo/losowane2.txt"));
+//        int hits = 0;
+//
+//        for (String elementLos1 : listLos1) {
+//            for (String elementLos2 : listLos2) {
+//                if (Integer.parseInt(elementLos1)==Integer.parseInt(elementLos2)){
+//                    hits++;
+//                }
+//            }
+//        }
+//
+//        System.out.println(hits);
+
+        //------------------ ZADANIE 4 -------------------------
+
+        Scanner tekstUzytkownika = new Scanner(System.in);
+       String tekst = tekstUzytkownika.nextLine();
+       char[] tekst1 = new char[tekst.length()];
+       for (int i = 0; i<tekst.length();i++){
+           tekst1[i] = tekst.charAt(i);
+       }
+
+       String tekstPo = "";
+
+       for (int i = tekst1.length ; i>0;i--){
+           tekstPo = tekstPo + tekst1[i-1];
+       }
+
+       System.out.println(tekstPo);
+    }
+
+
+    // druga wersja zadania 4
+
+    Scanner sc = new Scanner(System.in);
+
 }

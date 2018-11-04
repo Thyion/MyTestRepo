4d88c74df5911923e2d448b3819d9b8affce0948
Krzysztof <Krzysztof@DESKTOP-D70AKLV>
2018-11-04 13:07:03 +0100

    Homework

--- src/main/java/pl/infoshare/lesson5/Exercise3_4_5.java
+++ src/main/java/pl/infoshare/lesson5/Exercise3_4_5.java
@@ -1,107 +1,99 @@
 package pl.infoshare.lesson5;
 
-
-
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.*;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
-
-public class Exercise3_2  {
+public class Exercise3_4_5  {
 
     public static void main(String[] args) {
-        System.out.println("Wybierz 1 lub 2.");
-        System.out.println("1. Dodaj uzytkownika.\n2. Wyswietl uzytkownikow.");
         menu();
     }
 
     public static void menu() {
-
+        System.out.println("MENU !!");
+        System.out.println("Wybierz 1, 2 lub 0.");
+        System.out.println("1. Dodaj kolejnego uzytkownika.\n2. Wyświetl użytkowników.\n0. Zakończ program.");
         sprawdzLiczbe(podajLiczbe());
-
-//        try {
-//            ObjectOutputStream outS = new ObjectOutputStream(new GZIPOutputStream(new FileOutputStream("users2.txt"), true));
-//
-//            ObjectInputStream inS = new ObjectInputStream(new GZIPInputStream(new FileInputStream("users2.txt")));
-//
-//
-//            ArrayList usersFromFile = (ArrayList)inS.readObject();
-//
-//            usersFromFile.add(podajDaneUzytkownika());
-//            outS.writeObject(usersFromFile);
-//
-//            outS.close();
-//            inS.close();
-//        }
-//        catch (IOException e){
-//            System.out.println(e.getMessage());
-//        }
-//        catch (ClassNotFoundException ex) {
-//            System.out.println(ex.getMessage());
-//        }
+    }
+    public static void menu2() {
+        System.out.println("MENU !!");
+        System.out.println("Wybierz 1, 2 lub 0.");
+        System.out.println("1. Dodaj kolejnego uzytkownika.\n2. Wróć do menu.\n0. Zakończ program.");
+        sprawdzLiczbe2(podajLiczbe());
     }
 
     public static int podajLiczbe() {
-
         Scanner in = new Scanner(System.in);
         if(in.hasNextInt())
         return in.nextInt();
         else {
-            System.out.println("Miales podac liczbe 1 lub 2 ... Podaj jeszcze raz");
+            System.out.println("Miales podac liczbe ... Podaj jeszcze raz");
             return(podajLiczbe());
         }
     }
 
     public static User podajDaneUzytkownika() {
-
         return User.newUser();
-
     }
 
-    public static void sprawdzLiczbe(int i) {
-        int j = i;
-        switch (j){
-            case (1):
-                try {
+    public static void dodajUzytkownika() {
+        try {
                     ObjectOutputStream outS = new ObjectOutputStream(new GZIPOutputStream(new FileOutputStream("users2.txt"), true));
-
-
-                    ArrayList usersFromFile = new ArrayList();
-
                     usersFromFile.add(podajDaneUzytkownika());
-
                     outS.writeObject(usersFromFile);
-
                     outS.close();
-
                 } catch (IOException e) {
                     System.out.println(e.getMessage());
-                }
-
-            case (2):
-                System.out.println("asasasa");
-                try {
+                };         
+    }
+    
+    
+    public static void odczytajUzytkonikow() {
+        try {
                     ObjectInputStream inS = new ObjectInputStream(new GZIPInputStream(new FileInputStream("users2.txt")));
-
-
                     ArrayList usersFromFile = (ArrayList)inS.readObject();
-
                     inS.close();
                     System.out.println(usersFromFile);
-
-
+                    
                 } catch (IOException e) {
                     System.out.println(e.getMessage());
                 } catch (ClassNotFoundException ex) {
                     System.out.println(ex.getMessage());
-                }
-
-            default:
-                menu();
-        }
+                };
     }
+    
+    public static void sprawdzLiczbe(int i) {
+        if (i==1){
+              dodajUzytkownika();
+              menu2();
+        }
+        
+        else if(i==2){
+              odczytajUzytkonikow();  
+              menu();
+        }
+        else if(i==0){
+            System.exit(0);
+        } else {
+           menu();
+        }
+
+    }
+    public static void sprawdzLiczbe2(int i) {
+        if (i==1){
+              sprawdzLiczbe(1);      
+        }
+        else if(i==0){
+            System.exit(0);
+        } else {
+           menu();
+        }
+
+    }
+    public static ArrayList usersFromFile = new ArrayList();
 
 }
 
--- src/main/java/pl/infoshare/lesson5/User.java
+++ src/main/java/pl/infoshare/lesson5/User.java
@@ -11,6 +11,11 @@ public class User implements Serializable{
         this.userName = userName;
         this.userPassword = userPassword;
     }
+    public User(String userName, String userPassword, Boolean userType) {
+        this.userName = userName;
+        this.userPassword = userPassword;
+        this.userType = userType;
+    }
 
     public String getUserName() {
         return userName;
@@ -34,20 +39,42 @@ public class User implements Serializable{
         String name = in.nextLine();
         System.out.println("Podaj haslo dla uzytkownika " + name + ".");
         String password = in.nextLine();
-        User user = new User(name, password);
+        System.out.println("Podaj typ uzytkownika.");
+        boolean type = typeUser();
+        User user = new User(name, password, type);
         return user;
     }
-
+    
+    static public boolean typeUser() {
+        System.out.println("Jeśli admin to wpisz : ADMIN , Jeśli user to wpisz : USER");
+        Scanner in = new Scanner(System.in);
+        String type = in.nextLine();
+        if (type.equals("ADMIN")){
+            return true;
+        } else if (type.equals("USER")){
+            return false;
+        } else {
+            return typeUser();
+        }    
+    }
+    
+    public String getUserType() {
+        if (this.userType)
+            return "ADMIN";
+        else
+            return "USER";    
+    }
 
 
     @Override
     public String toString() {
 
-        return ("Uzytkownik: " + getUserName() + ", Haslo: " + getUserPassword());
+        return ("Uzytkownik: " + getUserName() + ", Haslo: " + getUserPassword() + ", Typ używkonika: " + getUserType() + "  |||||||||  ");
     }
 
     static int countUser;
     private String userName;
     private String userPassword;
+    private boolean userType;
 }
 

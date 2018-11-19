533f46a9437a6f0f1fa871a85920d8346f5e72cf
Thyion <krzbeszczynski@gmail.com>
2018-11-19 19:53:22 +0100

    Lesson 7

--- src/main/java/pl/infoshare/lesson9/homework/Homework.java
+++ src/main/java/pl/infoshare/lesson9/homework/Homework.java
@@ -1,4 +1,12 @@
 package pl.infoshare.lesson9.homework;
 
 public class Homework {
+
+
+//    Stwórz klasę, która będzie przetrzymywała pary dowolnych obiektów. (Jak w mapie)
+//
+//    Klasa musi mieć kolekcję, w której te pary będą przechowywane.
+//
+//    Użyj interfejsu, aby wystawić tylko niezbędną funkcjonalność.
+//
 }
---
+++ src/main/java/pl/infoshare/lesson9/homework/HomeworkList.java
@@ -0,0 +1,7 @@
+package pl.infoshare.lesson9.homework;
+
+public class HomeworkList {
+
+    private String[] pair = new String[2];
+
+}
